705ed8c7ee4457f1c89cfa034220b64cad1dc5fc
Mario Barrenechea <mbarrenechea@nasuni.com>
2022-01-05 20:53:04 -0500

    Juke email account configured to send emails.

--- juke_backend/settings.py
+++ juke_backend/settings.py
@@ -28,6 +28,12 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER=os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD=os.environ.get("EMAIL_HOST_PASSWORD")
+
 # Application definition
 
 INSTALLED_APPS = [
